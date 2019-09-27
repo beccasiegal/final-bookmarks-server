7da37034cbe3f26d6188e362b3020af13335725f
Rebecca Siegal <becca.siegal@gmail.com>
2019-09-27 11:19:28 -0400

    Added router info

---
+++ src/Bookmarks-router.js
@@ -0,0 +1,28 @@
+const BookmarksRouter = express.Router;
+BookmarksRouter
+    .route('/bookmark')
+    .post((req, res) => {})
+    
+
+BookmarksRouter
+ .route('/bookmark/:id')
+ .get((req,res)=> {
+    const { id } = req.params;
+    const bookmmark = bookmarks.find(c => b.id == id);
+  
+    // make sure we found a bookmark
+    if (!bookmark) {
+      logger.error(`Bookmark with id ${id} not found.`);
+      return res
+        .status(404)
+        .send('Bookmark Not Found');
+    }
+  
+    res.json(bookmark);
+ })
+ .delete((req, res)=> {})
+
+app.get('/bookmarks/:id', (req, res) => {
+  });
+
+  module.exports = BookmarksRouter
