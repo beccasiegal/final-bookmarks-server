f83e0ffd8c146f00edb8f1cf35d1a8e6980a4f4c
Rebecca Siegal <becca.siegal@gmail.com>
2019-10-02 09:24:39 -0400

    Updated post and delete endpoints

--- src/Bookmarks-router.js
+++ src/Bookmarks-router.js
@@ -1,29 +1,91 @@
-const express =require('express')
-const bookmark = require('../store')
+const express = require('express')
 
-const BookmarksRouter = express.Router;
+const BookmarksRouter = express.Router()
+const bodyParser = express.json()
+
 BookmarksRouter
-    .route('/bookmark')
-    .post((req, res) => {})
+  .route('/bookmarks')
+  .get((req, res) => {
+    app.get('/', (req, res) => {
+      res.send('Hello, bookmarks!')
+     })
+  })
+  .post(bodyParser, (req, res) => {
+    app.post('/list', (req, res) => {
+      const { header, Bookmarksids = [] } = req.body;
     
+      if (!header) {
+        logger.error(`Header is required`);
+        return res
+          .status(400)
+          .send('Invalid data');
+      }
+    
+      // check bookmarks IDs
+      if (BbookmarksIds.length > 0) {
+        let valid = true;
+        BookmarksIds.forEach(bid => {
+          const bookmark = bookmarks.find(c => b.id == bid);
+          if (!bookmarks) {
+            logger.error(`Bookmark with id ${bid} not found in bookmarks array.`);
+            valid = false;
+          }
+        });
+    
+        if (!valid) {
+          return res
+            .status(400)
+            .send('Invalid data');
+        }
+      }
+    
+      // get an id
+      const id = uuid();
+    
+      const list = {
+        id,
+        header,
+        BookmarksIds
+      };
+    
+      lists.push(list);
+    
+      logger.info(`List with id ${id} created`);
+    
+      res
+        .status(201)
+        .location(`http://localhost:8000/list/${id}`)
+        .json({id});
+    });
+  })
 
 BookmarksRouter
- .route('/bookmark/:id')
- .get((req,res)=> {
-    const { id } = req.params;
-    const bookmmark = bookmarks.find(c => b.id == id);
-  
-    // make sure we found a bookmark
-    if (!bookmark) {
-      logger.error(`Bookmark with id ${id} not found.`);
-      return res
-        .status(404)
-        .send('Bookmark Not Found');
-    }
-  
-    res.json(bookmark);
- })
- .delete((req, res)=> {})
+  .route('/bookmarks/:id')
+  .get((req, res) => {
+    app.get('/', (req, res) => {
+      res.send('Hello, bookmarks!')
+     })
+  })
+  .delete((req, res) => {
+    app.delete('/list/:id', (req, res) => {
+      const { id } = req.params;
+    
+      const listIndex = lists.findIndex(li => li.id == id);
+    
+      if (listIndex === -1) {
+        logger.error(`List with id ${id} not found.`);
+        return res
+          .status(404)
+          .send('Not Found');
+      }
+    
+      lists.splice(listIndex, 1);
+    
+      logger.info(`List with id ${id} deleted.`);
+      res
+        .status(204)
+        .end();
+    });
+  })
 
-
-  module.exports = BookmarksRouter
+module.exports = cardRouter
